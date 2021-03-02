bc2d1017a061c752b384fc7c31787d74fe0602fd
ho dev <hodev.dev@gmail.com>
2021-03-02 17:43:38 +0330

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={"text-white text-4xl bg-red-600 h-96"}>test</h1>
+    </div>
+  );
+}
+
+export default App;
