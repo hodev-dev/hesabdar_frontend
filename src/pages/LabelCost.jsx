08e4b003b580ad99de09c232ada13d15f507b9e9
ho dev <hodev.dev@gmail.com>
2021-03-21 09:50:07 +0330

    bug fices

--- src/pages/LabelCost.jsx
+++ src/pages/LabelCost.jsx
@@ -56,13 +56,13 @@ const LabelCost = (props) => {
     renderTable = costs.map((cost, index) => {
       return (
         <tr key={cost.id} className={"font-medium text-center hover:bg-gray-300 print:border print:border-gray-600"}>
-          <td className={"p-4 font-mono print:text-base print:hidden"}>{PN.convertEnToPe(Number(cost.id).toLocaleString())}</td>
-          <td className={"p-4 font-mono print:text-base"}>{PN.convertEnToPe(Number(cost.label.code).toLocaleString())}</td>
-          <td className={"p-4 font-mono print:text-base"}>{cost.section.name}</td>
-          <td className={"p-4 font-mono print:text-base"}>{PN.convertEnToPe(Number(cost.group_id)).toLocaleString()}</td>
-          <td className={"p-4 font-mono print:text-base"}>{PN.convertEnToPe(Number(cost.prev_value).toLocaleString())}</td>
-          <td className={"p-4 font-mono print:text-base"}>{PN.convertEnToPe(Number(cost.change).toLocaleString())}</td>
-          <td className={"p-4 font-mono print:text-base"}>{PN.convertEnToPe(Number(cost.final).toLocaleString())}</td>
+          <td className={"p-4 font-mono text-lg print:text-base print:hidden"}>{PN.convertEnToPe(Number(index + 1).toLocaleString())}</td>
+          <td className={"p-4 font-mono text-lg print:text-base print:hidden"}>{PN.convertEnToPe(Number(cost.label.code).toLocaleString())}</td>
+          <td className={"p-4 font-mono text-lg print:text-base"}>{cost.section.name}</td>
+          <td className={"p-4 font-mono text-lg print:text-base print:hidden"}>{PN.convertEnToPe(Number(cost.group_id)).toLocaleString()}</td>
+          <td className={"p-4 font-mono text-lg print:text-base"}>{PN.convertEnToPe(Number(cost.prev_value).toLocaleString())}</td>
+          <td className={"p-4 font-mono text-lg print:text-base"}>{PN.convertEnToPe(Number(cost.change).toLocaleString())}</td>
+          <td className={"p-4 font-mono text-lg print:text-base"}>{PN.convertEnToPe(Number(cost.final).toLocaleString())}</td>
           {/* <td className={"flex flex-col"}>
             <button onClick={() => 0} className={"w-full h-auto p-2 bg-yellow-300 border border-gray-200 border-none font-small hover:bg-yellow-500"}>ویرایش</button>
             <button onClick={() => 0} className={"w-full h-auto p-2 bg-red-300 border border-gray-200 border-none font-small hover:bg-yellow-500"}>حذف</button>
@@ -88,16 +88,16 @@ const LabelCost = (props) => {
             <h1 className={"w-6/12 text-center bg-white"}>شرکت نورد و لوله ی سپنتا اهواز - سهامی خاص</h1>
           </div>
           <div className={"flex justify-end w-full h-auto mt-2 bg-gray-300"}>
-            <h1 dir={"rtl"} className={"mr-16 text-3xl text-gray-600 print:mr-1 print:text-lg"}>{"0" + code + ' ' + '-' + ' ' + name}</h1>
+            <h1 dir={"rtl"} className={"mr-16 text-3xl text-gray-600 print:mr-1 print:text-lg"}>{"۰" + PN.convertEnToPe(code) + ' ' + '-' + ' ' + name}</h1>
           </div>
           <div className={"flex items-center justify-center w-full h-auto mt-4 bg-gray-300"}>
             <table className={"w-11/12 h-auto bg-gray-200 rounded-lg shadow-sm print:w-full"} dir={"rtl"}>
               <tbody>
                 <tr className={"text-gray-700 border border-gray-300 print:border print:border-black"}>
                   <th className={"p-4 font-bold print:text-base print:hidden"}>ردیف</th>
-                  <th className={"p-4 font-bold print:text-base"}>کد شرح</th>
+                  <th className={"p-4 font-bold print:text-base print:hidden"}>کد شرح</th>
                   <th className={"p-4 font-bold print:text-base"}>شرح هزینه</th>
-                  <th className={"p-4 font-bold print:text-base"}>گروه</th>
+                  <th className={"p-4 font-bold print:text-base print:hidden"}>گروه</th>
                   <th className={"p-4 font-bold print:text-base"}>هزینه به ریال</th>
                   <th className={"p-4 font-bold print:text-base"}>تغییرات تسهیم</th>
                   <th className={"p-4 font-bold print:text-base"}>مانده به ریال</th>
@@ -109,10 +109,10 @@ const LabelCost = (props) => {
           </div>
           <div className={"flex flex-col items-center justify-center w-full h-auto mt-2 bg-gray-300 print:justify-start"}>
             <div className={"w-11/12 h-auto p-4 bg-gray-200 rounded-lg shadow-sm print:w-full print:border print:border-gray-600"} dir={"rtl"}>
-              <h1 className={'font-mono text-base'}>{'جمع معین' + ' ' + PN.convertEnToPe(Number(sum).toLocaleString()) + ' ' + 'ریال'}</h1>
+              <h1 className={'font-mono text-lg'}>{'جمع هزینه اولیه' + ' ' + PN.convertEnToPe(Number(sum).toLocaleString()) + ' ' + 'ریال'}</h1>
             </div>
             <div className={"w-11/12 h-auto p-4 mt-2 bg-gray-200 rounded-lg shadow-sm print:w-full print:border print:border-gray-600"} dir={"rtl"}>
-              <h1 className={'font-mono text-base'}>{'جمع کل' + ' ' + PN.convertEnToPe(Number(rangeSum).toLocaleString()) + ' ' + 'ریال'}</h1>
+              <h1 className={'font-mono text-lg'}>{'جمع مانده' + ' ' + PN.convertEnToPe(Number(rangeSum).toLocaleString()) + ' ' + 'ریال'}</h1>
             </div>
           </div>
         </div>
